83f6617adc166185d9d09b802d7ab8a7fca5d994
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-09-04 17:40:04 -0600

    Add List

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+
+  get 'lists/show'
+
+  get 'lists/new'
+
+  get 'lists/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
