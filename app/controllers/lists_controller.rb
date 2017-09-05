8b94d1b4a1444acaf898e6ef38bb8f3d275bc761
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-09-04 20:00:52 -0600

    Destroy List New List Link

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,7 +14,11 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
   def create
     @list = List.new(list_params)
     if @list.save
